3a9d6f206c5e843b67e8577bd9af81cc949fd1fa
DillerOFire <niktofe1@gmail.com>
2023-10-17 15:29:14 +1000

    eqs: Switch to 64bit only build

--- hentai_eqs.mk
+++ hentai_eqs.mk
@@ -15,8 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-ZYGOTE_FORCE_64 := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from eqs device
