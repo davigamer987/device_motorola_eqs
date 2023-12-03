7f5f589190cccdf90786ab421a6b6f9d2fe41632
Nishant Kumar <www.rajsonu13@gmail.com>
2023-12-03 09:08:53 +0530

    eqs: Enable 32bit zygote

--- aosp_eqs.mk
+++ aosp_eqs.mk
@@ -15,7 +15,6 @@
 #
 
 # Inherit from those products. Most specific first.
-ZYGOTE_FORCE_64 := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
