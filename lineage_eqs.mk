4a0ba3e53c6e4532b630dc5f8e3f8946bf2a3889
ROMSG <romsg85@gmail.com>
2024-10-20 21:27:19 -0700

    eqs: Adjust override prop key names
    Change-Id: I7c2666496297e7db62d77b885002311c8658c441

--- lineage_eqs.mk
+++ lineage_eqs.mk
@@ -33,14 +33,12 @@ PRODUCT_MODEL := motorola edge 30 ultra
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=eqs_ge \
-    PRIVATE_BUILD_DESC="eqs_ge-user 14 U1SQS34.52-21-1-7 05c162-3a3ec6 release-keys"
-
-BUILD_FINGERPRINT := motorola/eqs_ge/msi:14/U1SQS34.52-21-1-7/05c162-3a3ec6:user/release-keys
+    DeviceProduct="eqs_ge" \
+    BuildDesc="eqs_ge-user 14 U1SQS34.52-21-1-10 504bf-893e0 release-keys" \
+    BuildFingerprint="motorola/eqs_ge/eqs:14/U1SQS34.52-21-1-10/504bf-893e0:user/release-keys"
 
 # RisingOS Flags
 WITH_GMS := true
-# TARGET_CORE_GMS := true
 TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_HAS_UDFPS := true
