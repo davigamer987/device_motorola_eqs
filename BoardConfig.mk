594ed3c4689c9ac2abb6ca923e8bbc2669c3014b
davigamer987 <davimarchesiprimo@gmail.com>
2024-12-03 02:58:50 +0100

    eqs: update flags and props

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
 
 # Security
-VENDOR_SECURITY_PATCH := 2999-12-31
+VENDOR_SECURITY_PATCH := 2024-08-01
 
 # inherit from the proprietary version
 include vendor/motorola/eqs/BoardConfigVendor.mk
